e7a0f179a37d37db66235c76f3ec8f77fb8ca431
00Calueto <paulo.calueto.francisco@gmail.com>
2026-01-03 11:05:00 +0100

    new schedule model

---
+++ app/Models/Schedule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    /** @use HasFactory<\Database\Factories\ScheduleFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'doctor_id',
+        'day_weeks',
+        'start_time',
+        'end_time',
+        'is_available'
+    ];
+
+    public function doctor(){
+        return $this->belongsTo(Doctor::class,'doctor_id');
+    }
+}
