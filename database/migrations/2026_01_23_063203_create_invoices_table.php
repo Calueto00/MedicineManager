678af69f67248f7bc607c9c23db4efb79c05d207
00Calueto <paulo.calueto.francisco@gmail.com>
2026-01-30 14:34:53 +0100

    invoice migration

---
+++ database/migrations/2026_01_23_063203_create_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('appointment_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('patient_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('doctor_id')->constrained()->cascadeOnDelete();
+            $table->decimal('amount',10, 2);
+            $table->enum('payment_method',['cash','card','transfer','mobile_money']);
+            $table->enum('status',['pending','paid','cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
