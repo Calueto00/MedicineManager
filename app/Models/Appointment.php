8d5850933d683965317998806125e13474d33fc2
00Calueto <paulo.calueto.francisco@gmail.com>
2026-01-30 14:32:46 +0100

    invoice on appointment

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -19,10 +19,21 @@ class Appointment extends Model
     ];
 
     public function patient(){
-        return $this->belongsTo(Patients::class, 'patient_id');
+        return $this->belongsTo(Patients::class);
     }
 
     public function schedule(){
         return $this->belongsTo(Schedule::class,'schedule_id');
     }
+
+    public function exams(){
+        return $this->hasMany(Exam_Request::class);
+    }
+
+    public function invoice()
+    {
+        return $this->hasOne(Invoice::class);
+    }
+
+
 }
